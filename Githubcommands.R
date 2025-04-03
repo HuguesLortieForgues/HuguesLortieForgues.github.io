4608f5abc65d4a988aa809ee800e0fb27ffb6257
HuguesLortieForgues <lfhugues@gmail.com>
2025-04-03 15:30:36 +0100

    initial commit

---
+++ Githubcommands.R
@@ -0,0 +1,9 @@
+git remote add origin https://github.com/HuguesLortieForgues/HuguesLortieForgues.github.io
+git branch -M main
+git add --all
+git commit -m "initial commit"
+git push -u origin main
+
+
+git config --global user.email "lfhugues@gmail.com"
+git config --global user.name "HuguesLortieForgues"
